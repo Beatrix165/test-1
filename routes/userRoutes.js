47f2d15c4046f028562b285e5105ec64b4c743bd
Prabhav Singh <65345194+prabhavsingh@users.noreply.github.com>
2025-03-25 04:46:08 +0530

    minor change

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,7 +15,7 @@ router.patch('/resetPassword/:token', authController.resetPassword);
 //Protect all routes after this middleware
 router.use(authController.protect);
 
-router.patch('/updateMyPassword', authController.updatePassword);
+router.patch('/updateMyPassword', authController.updatepassword);
 
 router.get('/me', userController.getMe, userController.getUser);
 router.patch(
